e710958a8782b43ee3b28767464c718c893af4cc
CarolGF <karola_gf@hotmail.com>
2019-10-28 15:26:58 +0000

    Adding all files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz"
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "antonette",
+    email: "Shanna@nelissa.tv"
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Kathan@yesenia.net"
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@koru.org"
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Lammen",
+    email: "Lucio_Hettinger@annie.ca"
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulz",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info"
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skilles",
+    email: "Telly.hoeger@billy.biz"
+  },
+  {
+    id: 8,
+    name: "Nicholas Bruno",
+    username: "Maxime.nim",
+    email: "sherwood@rosanne.se"
+  },
+  {
+    id: 9,
+    name: "Glenn Reichert",
+    username: "Delphine",
+    email: "chain_McDermont@dana.io"
+  },
+  {
+    id: 10,
+    name: "Clement DuBuque",
+    username: "Moriah_State",
+    email: "Rey.Padberg@harina.biz"
+  }
+];
