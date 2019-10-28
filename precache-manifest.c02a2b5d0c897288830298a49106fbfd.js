3a0f4ab6ad0c19a069618197377bf4f8362e2b4d
karola_gf@hotmail.com <karola_gf@hotmail.com>
2019-10-28 16:11:15 +0000

    Updates

---
+++ precache-manifest.c02a2b5d0c897288830298a49106fbfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0062c2dcbbcdbf96bbb4cdd68db9bfe4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "be4179a417a9321fae24",
+    "url": "/robofriends/static/css/main.9773d44b.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "be4179a417a9321fae24",
+    "url": "/robofriends/static/js/main.48028ce0.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
